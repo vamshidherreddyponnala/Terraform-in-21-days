7cdb323295fdd9762fe12fc85dbaff6510f83b59
Vamshidher Reddy Ponnala <124044522+vamshitynybay@users.noreply.github.com>
2023-06-05 13:29:25 +0530

    Use datasources (#9)

--- instance.tf
+++ instance.tf
@@ -1,8 +1,24 @@
+data "aws_ami" "amazonlinux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = [ "amzn2-ami-kernel-*" ]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  
+}
+
 resource "aws_instance" "public" {
-  ami                         = "ami-0cff7528ff583bf9a"
+  ami                         = data.aws_ami.amazonlinux.id
   associate_public_ip_address = true
   instance_type               = "t3.micro"
-  key_name                    = "Key_Pair"
+  key_name                    = "Key_Pair_Mumbai"
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = aws_subnet.public[0].id
 
@@ -36,9 +52,9 @@ resource "aws_security_group" "public" {
 }
 
 resource "aws_instance" "private" {
-  ami                    = "ami-0cff7528ff583bf9a"
+  ami                    = data.aws_ami.amazonlinux.id
   instance_type          = "t3.micro"
-  key_name               = "Key_Pair"
+  key_name               = "Key_Pair_Mumbai"
   vpc_security_group_ids = [aws_security_group.private.id]
   subnet_id              = aws_subnet.private[0].id
 
