78e208bba1d49d15aacfb3b7af64c03c8075a61d
“vamshidher <vamshidher.ponnala@tynybay.com>
2023-06-15 15:46:40 +0530

    install http

--- instance.tf
+++ instance.tf
@@ -39,7 +39,13 @@ resource "aws_security_group" "public" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    description = "HTTP from public"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
