af6ad5af9c235046bdd5c0c7d606e4e1523db0f9
Vamshidher Reddy Ponnala <124044522+vamshitynybay@users.noreply.github.com>
2023-06-15 15:58:01 +0530

    User data2 (#12)
    * user-data2
    
    * install http

--- instance.tf
+++ instance.tf
@@ -39,7 +39,13 @@ resource "aws_security_group" "public" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+  ingress {
+    description = "HTTP from public"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
--- user-data.sh
+++ user-data.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# yum update -y
+# yum install -y httpd git
+# git clone https://github.com/gabrielecirulli/2048.git
+# cp -R 2048/* /var/www/html/
+# systemctl start httpd && systemctl enable httpd
+
 yum update -y
-yum install -y httpd git
-git clone https://github.com/gabrielecirulli/2048.git
-cp -R 2048/* /var/www/html/
+yum install -y httpd
 systemctl start httpd && systemctl enable httpd
