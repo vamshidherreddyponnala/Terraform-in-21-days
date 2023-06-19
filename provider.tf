c6cd82a7284ae3769ab8ae71cf2f927e37eb084f
Vamshidher Reddy Ponnala <124044522+vamshitynybay@users.noreply.github.com>
2023-06-19 15:31:48 +0530

    Configure remote state (#15)
    * Add initial changes2
    
    * Key_Pair
    
    * Configure remote state
    
    * Confirm lock table
    
    * save

--- provider.tf
+++ provider.tf
@@ -1,5 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-remote-256"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-remote-2"
+  }
+}
+
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 
