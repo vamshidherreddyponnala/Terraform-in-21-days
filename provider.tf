54d295998b7d9deb0ed2a47e82f25bee0e7668fd
“vamshidher <vamshidher.ponnala@tynybay.com>
2023-06-19 14:34:41 +0530

    Configure remote state

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-remote-256"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
